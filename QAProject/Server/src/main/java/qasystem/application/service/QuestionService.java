ae00af97c30b1b874ddc879289612547a524fdd3
Sebastian <s.karl625@gmx.de>
2017-07-03 15:41:16 +0200

    Methodenrümpfe in QuestionController eingefügt. Funktionalität zum Abfragen aller Fragen hinzugefügt.

--- QAProject/Server/src/main/java/qasystem/application/service/QuestionService.java
+++ QAProject/Server/src/main/java/qasystem/application/service/QuestionService.java
@@ -1,7 +1,39 @@
 package qasystem.application.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import qasystem.persistence.entities.Question;
+import qasystem.persistence.repositories.QuestionRepository;
+import qasystem.web.dtos.QuestionDTO;
+import qasystem.web.dtos.UserDTO;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    public List<QuestionDTO> getAllQuestions() {
+        List<Question> all = questionRepository.findAll();
+        return convertListToDTOs(all);
+    }
+
+    private List<QuestionDTO> convertListToDTOs(List<Question> all) {
+        List<QuestionDTO> allDTOs = new LinkedList<>();
+        for(Question q: all){
+            QuestionDTO newDTO = new QuestionDTO();
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserName(q.getUser().getUsername());
+            userDTO.setId(q.getUser().getId());
+            newDTO.setId(q.getId());
+            newDTO.setTitle(q.getTitle());
+            newDTO.setText(q.getText());
+            newDTO.setUser(userDTO);
+            allDTOs.add(newDTO);
+        }
+        return allDTOs;
+    }
 }
--- QAProject/Server/src/main/java/qasystem/web/controller/QuestionController.java
+++ QAProject/Server/src/main/java/qasystem/web/controller/QuestionController.java
@@ -1,9 +1,12 @@
 package qasystem.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import qasystem.application.service.QuestionService;
+import qasystem.web.dtos.QuestionDTO;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/questions")
@@ -14,4 +17,48 @@ public class QuestionController {
     public QuestionController(QuestionService questionService){
         this.questionService = questionService;
     }
+
+    //=======GET-MAPPING=======
+    @GetMapping(value = "/all")
+    public List<QuestionDTO> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping(value = "/unanswered")
+    public List<QuestionDTO> getAllUnansweredQuestions(){
+        return null;
+    }
+
+    @GetMapping(value = "/unsolved")
+    public List<QuestionDTO> getAllUnsolvedQuestions(){
+        return null;
+    }
+
+    @GetMapping(value = "/{id:[1-9]+}/answers")
+    public QuestionDTO getAllAnswersByQuestionId(@PathVariable("id") String id){
+        return null;
+    }
+
+    //=======POST-MAPPING=======
+    @PostMapping(value = "/{id:[1-9]+}/answers/newAnswer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void answerQuestion(@PathVariable("id") String id){
+
+    }
+
+    //=======PUT-MAPPING=======
+    @PutMapping(value = "/{id:[1-9]+}/answers/{aId:[1-9]+}")
+    @ResponseStatus(HttpStatus.OK)
+    public void acceptAnswer(@PathVariable("id") String id, @PathVariable("aId") String aId){
+
+    }
+
+    //=======DELETE-MAPPING=======
+    @DeleteMapping (value = "/{id:[1-9]+}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteQuestion(@PathVariable("id") String id){}
+
+    @DeleteMapping (value = "/{id:[1-9]+}/answers/{aId:[1-9]+}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteAnswer(@PathVariable("id") String id, @PathVariable("aId") String aId){}
 }
