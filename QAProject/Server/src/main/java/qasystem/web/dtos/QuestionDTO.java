7ad108252aeb28d3cf894dde1d40606e8668d08b
Sebastian <s.karl625@gmx.de>
2017-07-03 15:37:08 +0200

    Attribute, Getter und Setter zu DTOs hinzugefügt.

--- QAProject/Server/src/main/java/qasystem/web/dtos/QuestionDTO.java
+++ QAProject/Server/src/main/java/qasystem/web/dtos/QuestionDTO.java
@@ -1,7 +1,85 @@
 package qasystem.web.dtos;
 
-/**
- * Created by a on 29.06.2017.
- */
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import java.util.Collection;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
 public class QuestionDTO {
+    private long id;
+
+    @NotNull
+    @NotEmpty
+    private String title;
+
+    @NotNull
+    @NotEmpty
+    private String text;
+
+    private GregorianCalendar date;
+
+    private boolean answered;
+
+    private UserDTO user;
+
+    private Collection<AnswerDTO> answers = new LinkedList<>();
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public void setDate(GregorianCalendar date) {
+        this.date = date;
+    }
+
+    public boolean isAnswered() {
+        return answered;
+    }
+
+    public void setAnswered(boolean answered) {
+        this.answered = answered;
+    }
+
+    public UserDTO getUser() {
+        return user;
+    }
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public Collection<AnswerDTO> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Collection<AnswerDTO> answers) {
+        this.answers = answers;
+    }
 }
--- QAProject/Server/src/main/java/qasystem/web/dtos/UserDTO.java
+++ QAProject/Server/src/main/java/qasystem/web/dtos/UserDTO.java
@@ -8,12 +8,22 @@ import javax.validation.constraints.NotNull;
  * DTO zum Kapseln der Daten eines Benutzers.
  */
 public class UserDTO {
+
+    private long id;
     @NotNull
     @NotEmpty
     private String userName;
     @NotNull
     @NotEmpty
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getUserName() {
         return userName;
