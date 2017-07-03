6c876c71370251347181c06bd7a08b98e41bc6d4
Sebastian <s.karl625@gmx.de>
2017-07-04 01:00:37 +0200

    SQL Fix (ohne Gewähr)

--- QAProject/Server/src/main/java/qasystem/persistence/repositories/QuestionRepository.java
+++ QAProject/Server/src/main/java/qasystem/persistence/repositories/QuestionRepository.java
@@ -20,8 +20,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     Collection<Question> findAllByUserId(@Param("UserId") long UserId);
 
     // why dis no work
-//    @Query(value = "select q from Question q except (select distinct q2 from Question q2, Answer a where a.parentQuestion = q2)")
-//    Collection<Question> findAllByNoReply();
+    @Query(value = "select q from Question q where q.id not in (select distinct q2.id from Question q2, Answer a where a.parentQuestion = q2)")
+    Collection<Question> findAllByNoReply();
 
     @Query(value = "update Question q set q.answered = :answered where q.id = :questionId")
     Collection<Question> updateAnswered(@Param("questionId") long questionId, @Param("answered") boolean answered);
