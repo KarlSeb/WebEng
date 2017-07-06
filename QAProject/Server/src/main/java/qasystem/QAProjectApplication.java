081cd08e2beb1c71cb62272a10ea95cc041eba6a
Sebastian <s.karl625@gmx.de>
2017-07-06 22:30:27 +0200

    Imports ausgemistet.

--- QAProject/Server/src/main/java/qasystem/QAProjectApplication.java
+++ QAProject/Server/src/main/java/qasystem/QAProjectApplication.java
@@ -1,15 +1,7 @@
 package qasystem;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import qasystem.persistence.entities.User;
-import qasystem.persistence.repositories.UserRepository;
-import qasystem.web.controller.GenericController;
-import qasystem.web.dtos.UserDTO;
 
 /**
  * Startet das System per Spring Boot.
